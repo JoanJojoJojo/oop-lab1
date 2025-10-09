a9c5181d0b08891553b2b0768285f7d2a711cadb
JoanJojoJojo <kyokusiimajoan00@gmail.com>
2025-10-09 22:34:50 +0300

    Add files via upload

---
+++ hostel.py
@@ -0,0 +1,27 @@
+class Resident:
+    def __init__(self, name, age, room_number):
+        self.name = name
+        self.age = age
+        self.room_number = room_number
+
+class Visitor:
+    def __init__(self, name, visit_date, resident_name, phone_number):
+        self.name = name
+        self.visit_date = visit_date
+        self.resident_name = resident_name
+        self.phone_number = phone_number
+
+class Hostel:
+    def __init__(self, name, address):
+        self.name = name
+        self.address = address
+class security:
+        def __init__(self,  name, age, employee_id):
+            self.name = name
+            self.age = age
+            self.employee_id = employee_id
+
+        
+
+
+        
