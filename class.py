8c6ee0bd3807dd9c15f45f666abf952d13ab91d9
JoanJojoJojo <kyokusiimajoan00@gmail.com>
2025-10-09 22:35:24 +0300

    Add files via upload

---
+++ class.py
@@ -0,0 +1,39 @@
+class student:
+    def __init__(self, name):              
+        self.name = name                    #public
+        self._gpa =3.5                     #protected
+        self.__password = '1234'           #private
+            
+student1 = student("Joela jacobs")
+student1.name
+student1._gpa
+
+print student1.name
+
+coins = 100
+ coins = 100 + 500
+
+print(" I have:", coins, "coins")
+
+
+
+
+
+    
+
+
+    
+    
+
+
+    
+
+
+
+
+
+
+
+
+
+
